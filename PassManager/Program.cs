da7deb822b2da57d11d706dc95b201b1f3bf8ebb
mihan <mihan@DESKTOP-MU77ACG>
2023-12-10 16:29:10 +0200

    Add project files.

---
+++ PassManager/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassManager
+{
+    static class Program
+    {
+        public static bool CorrectPassword = false;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            if (CorrectPassword)
+                Application.Run(new MainForm());
+        }
+    }
+}
